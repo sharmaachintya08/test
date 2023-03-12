8c26cc90fdbc3a4abc80c5c86ed9bb2d6b0e7875
Achintya Sharma <sharmaachintya08@gmail.com>
2023-03-12 16:12:32 +0530

    created recyclerAdapter class but didnt add to the fragment

---
+++ app/src/main/java/com/example/emptyactivity/Fragments/RecyclerView/RecyclerAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.emptyactivity.Fragments.RecyclerView
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.emptyactivity.R
+
+
+class RecyclerAdapter(private val dataSet : Array<DataModel>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val songImage : ImageView
+        val songName : TextView
+        val songTime : TextView
+        val songSinger : TextView
+        val songMenu : ImageView
+        init{
+            songImage = itemView.findViewById(R.id.songImage)
+            songName = itemView.findViewById(R.id.songName)
+            songTime = itemView.findViewById(R.id.songTime)
+            songSinger = itemView.findViewById(R.id.songSinger)
+            songMenu = itemView.findViewById(R.id.songMenu)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.song_card,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {return dataSet.size}
+}
