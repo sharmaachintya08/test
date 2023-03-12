1ada1335e97be1838b9b47db6f5b92f64fe2893b
Achintya Sharma <sharmaachintya08@gmail.com>
2023-03-12 22:28:25 +0530

    made upper layout but not pretty

--- app/src/main/java/com/example/emptyactivity/MainActivity.kt
+++ app/src/main/java/com/example/emptyactivity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.example.emptyactivity.Fragments.MainScreen.SongsList
+import com.example.emptyactivity.Fragments.SongPlay.SongPlaying
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,12 +15,15 @@ class MainActivity : AppCompatActivity() {
 
         displayFragment()
     }
-    private fun newInstance() : SongsList{
+    private fun songListInstance() : SongsList{
         return SongsList()
+    }
+    private fun songPlayInstance() : SongPlaying{
+        return SongPlaying()
     }
     private fun displayFragment(){
         supportFragmentManager.beginTransaction()
-            .add(R.id.upper_fragment,newInstance())
+            .add(R.id.upper_fragment,songPlayInstance())
             .addToBackStack("songListFragment")
             .commit()
     }
