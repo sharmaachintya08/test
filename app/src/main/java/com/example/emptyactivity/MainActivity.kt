17277360c005c4ef451045591066222d4e567687
Achintya Sharma <sharmaachintya08@gmail.com>
2023-03-12 19:22:54 +0530

    added fragment to the activity

--- app/src/main/java/com/example/emptyactivity/MainActivity.kt
+++ app/src/main/java/com/example/emptyactivity/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.emptyactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.example.emptyactivity.Fragments.MainScreen.SongsList
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var firstUpperFragment : Fragment? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        displayFragment()
+    }
+    private fun newInstance() : SongsList{
+        return SongsList()
+    }
+    private fun displayFragment(){
+        supportFragmentManager.beginTransaction()
+            .add(R.id.upper_fragment,newInstance())
+            .addToBackStack("songListFragment")
+            .commit()
     }
 }
