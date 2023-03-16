a230c4a1bcc0e3748094c0a40f0c47f6a98acb64
Achintya Sharma <sharmaachintya08@gmail.com>
2023-03-16 18:34:21 +0530

    added function getMusic() to collect music from storage - not working

--- app/src/main/java/com/example/emptyactivity/Fragments/MainScreen/SongsList.kt
+++ app/src/main/java/com/example/emptyactivity/Fragments/MainScreen/SongsList.kt
@@ -1,22 +1,30 @@
 package com.example.emptyactivity.Fragments.MainScreen
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract.Data
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.emptyactivity.Fragments.RecyclerView.DataModel
 import com.example.emptyactivity.Fragments.RecyclerView.RecyclerAdapter
 import com.example.emptyactivity.R
-
+import java.io.File
 
 
 class SongsList : Fragment() {
 
+    companion object{
+        lateinit var musicList : ArrayList<DataModel>
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,22 +33,42 @@ class SongsList : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_songs_list, container, false)
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(context)
-        val dataSet = ArrayList<DataModel>()
-
-        dataSet.add(DataModel(null,"song1","2:34","achintya"))
-        dataSet.add(DataModel(null,"song1","2:34","achintya"))
-        dataSet.add(DataModel(null,"song1","2:34","achintya"))
-        dataSet.add(DataModel(null,"song1","2:34","achintya"))
-        dataSet.add(DataModel(null,"song1","2:34","achintya"))
-        dataSet.add(DataModel(null,"song1","2:34","achintya"))
-        dataSet.add(DataModel(null,"song1","2:34","achintya"))
-        dataSet.add(DataModel(null,"song1","2:34","achintya"))
-        dataSet.add(DataModel(null,"song1","2:34","achintya"))
-        dataSet.add(DataModel(null,"song1","2:34","achintya"))
-        dataSet.add(DataModel(null,"song1","2:34","achintya"))
-        dataSet.add(DataModel(null,"song1","2:34","achintya"))
-
-        recyclerView.adapter = RecyclerAdapter(dataSet)
+        musicList = getMusic()
+        recyclerView.adapter = RecyclerAdapter(musicList)
         return view
+    }
+
+
+    fun getMusic() : ArrayList<DataModel>{
+        val tempList = ArrayList<DataModel>()
+        val selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0"
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.DURATION,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.DATA,
+            MediaStore.Audio.Media.DATE_ADDED
+        )
+        val cursor  =  context?.contentResolver?.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,projection,selection,null,
+            MediaStore.Audio.Media.DATE_ADDED + " DESC",null)
+        if(cursor != null){
+            if(cursor.moveToFirst()){
+                do{
+                    val title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE))
+                    val id = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
+                    val artist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
+                    val duration = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION))
+                    val path = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))
+                    val album = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM))
+                    val music = DataModel(songImage = album,songId = id,songName = title, songTime = duration, songArtist = artist, songPath = path)
+                    val file = File(music.songPath)
+                    if(file.exists()) tempList.add(music)
+                }while(cursor.moveToNext())
+            }
+        }
+        return tempList
     }
 }
