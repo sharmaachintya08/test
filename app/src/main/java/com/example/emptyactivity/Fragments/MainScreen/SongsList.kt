5c281d7907a3e085cd52917e7ff4f2e376e51f05
Achintya Sharma <sharmaachintya08@gmail.com>
2023-03-18 02:51:12 +0530

    project complete

--- app/src/main/java/com/example/emptyactivity/Fragments/MainScreen/SongsList.kt
+++ app/src/main/java/com/example/emptyactivity/Fragments/MainScreen/SongsList.kt
@@ -17,6 +17,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.emptyactivity.Fragments.RecyclerView.DataModel
 import com.example.emptyactivity.Fragments.RecyclerView.RecyclerAdapter
+import com.example.emptyactivity.MainActivity
 import com.example.emptyactivity.R
 import java.io.File
 
@@ -34,47 +35,8 @@ class SongsList : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_songs_list, container, false)
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(context)
-        musicList = getMusic()
+        musicList = MainActivity.tempList
         recyclerView.adapter = RecyclerAdapter(musicList)
         return view
-    }
-
-
-    fun getMusic() : ArrayList<DataModel>{
-        val tempList = ArrayList<DataModel>()
-        val selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0"
-        val projection = arrayOf(
-            MediaStore.Audio.Media._ID,
-            MediaStore.Audio.Media.TITLE,
-            MediaStore.Audio.Media.ARTIST,
-            MediaStore.Audio.Media.DURATION,
-            MediaStore.Audio.Media.ALBUM,
-            MediaStore.Audio.Media.DATA,
-            MediaStore.Audio.Media.DATE_ADDED
-        )
-        val cursor  =  context?.contentResolver?.query(
-            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-            projection,
-            selection,
-            null,
-            MediaStore.Audio.Media.DATE_ADDED + " DESC",
-            null
-        )?.use{ cursor ->
-            while(cursor.moveToNext()){
-                val title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE))
-                val id = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
-                val artist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
-                val duration = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION))
-                val path = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))
-                val album = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM))
-                val music = DataModel(songImage = album,songId = id,songName = title, songTime = duration, songArtist = artist, songPath = path)
-                val file = File(music.songPath)
-                if(file.exists()) tempList.add(music)
-            }
-        }
-        return tempList
-    }
-    fun getMusicF(){
-
     }
 }
--- app/src/main/java/com/example/emptyactivity/MainActivity.kt
+++ app/src/main/java/com/example/emptyactivity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.emptyactivity
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -15,15 +16,19 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.emptyactivity.Fragments.BottomBar
 import com.example.emptyactivity.Fragments.MainScreen.SongsList
+import com.example.emptyactivity.Fragments.RecyclerView.DataModel
 import com.example.emptyactivity.Fragments.SongPlay.SongPlaying
+import java.io.File
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    companion object{
+        lateinit var tempList : ArrayList<DataModel>
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        tempList = getMusic()
         displayFragment()
     }
     fun displayFragment(){
@@ -31,5 +36,39 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.upper_fragment,SongsList())
             .addToBackStack("songListFragment")
             .commit()
+    }
+    fun getMusic() : ArrayList<DataModel>{
+        val tempList = ArrayList<DataModel>()
+        val selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0"
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.DURATION,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.DATA,
+            MediaStore.Audio.Media.DATE_ADDED
+        )
+        val cursor  =  applicationContext.contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            null,
+            MediaStore.Audio.Media.DATE_ADDED + " DESC",
+            null
+        )?.use{ cursor ->
+            while(cursor.moveToNext()){
+                val title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE))
+                val id = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
+                val artist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
+                val duration = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION))
+                val path = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))
+                val album = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM))
+                val music = DataModel(songImage = album,songId = id,songName = title, songTime = duration, songArtist = artist, songPath = path)
+                val file = File(music.songPath)
+                if(file.exists()) tempList.add(music)
+            }
+        }
+        return tempList
     }
 }
